510b2c6b67a47732170c100f36ac90e93e5e1723
djcranney <danieljamescranney@gmail.com>
2021-09-19 19:42:21 +0100

    Icon Updates

--- components/icons/Firebase.js
+++ components/icons/Firebase.js
@@ -4,7 +4,7 @@ export default function Firebase() {
   return (
     <svg viewBox="0 0 128 128">
       <path
-        className="text-white fill-current opacity-20 group-hover:opacity-100 transition-all duration-200 group-hover:text-brand"
+        className="text-white transition-all duration-200 ease-in-out fill-current opacity-20 group-hover:opacity-100 group-hover:text-brand"
         d="M27.35 80.52l10.68-68.44c.37-2.33 3.5-2.89 4.6-.8l11.48 21.48-26.76 47.76zm75.94 16.63L93.1 34.11c-.31-1.96-2.76-2.76-4.17-1.35L24.71 97.15l35.54 19.95a7.447 7.447 0 007.18 0l35.86-19.95zm-28.85-55L66.21 26.5c-.92-1.78-3.44-1.78-4.36 0L25.7 90.95l48.74-48.8z"
       ></path>
     </svg>
