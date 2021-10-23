2267dbd9fe98b96127ae3678703da76b5fd77d94
djcranney <danieljamescranney@gmail.com>
2021-10-23 15:39:43 +0100

    Profile Picture Update

--- pages/index.js
+++ pages/index.js
@@ -576,7 +576,7 @@ export default function Home() {
               </div>
               <div className="flex w-full h-full mb-4 md:pl-8 md:w-2/5 md:mb-0">
                 <Image
-                  src="/headshot-with-frame.jpg"
+                  src="/headshot-with-frame-2.jpg"
                   className="overflow-hidden rounded-md"
                   width={880}
                   height={880}
